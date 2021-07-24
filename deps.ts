ad7b67c5a01f1d9e332bc875150c07c4d7f116c0
liquidz <liquidz.uo@gmail.com>
2021-07-25 06:27:16 +0900

    Bump deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,7 @@
-export { BufReader, BufWriter } from "https://deno.land/std@0.99.0/io/bufio.ts";
-export { StringReader } from "https://deno.land/std@0.99.0/io/readers.ts";
-export { StringWriter } from "https://deno.land/std@0.99.0/io/writers.ts";
-export * as asserts from "https://deno.land/std@0.99.0/testing/asserts.ts";
+export {
+  BufReader,
+  BufWriter,
+} from "https://deno.land/std@0.102.0/io/bufio.ts";
+export { StringReader } from "https://deno.land/std@0.102.0/io/readers.ts";
+export { StringWriter } from "https://deno.land/std@0.102.0/io/writers.ts";
+export * as asserts from "https://deno.land/std@0.102.0/testing/asserts.ts";
