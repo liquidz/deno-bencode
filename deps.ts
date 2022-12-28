605f0306ec64469ddb90d3c3749862dbb0d10749
liquidz <liquidz.uo@gmail.com>
2022-12-29 07:55:54 +0900

    deps: Bump outdated deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { BufReader, BufWriter } from "https://deno.land/std@0.167.0/io/mod.ts";
-export { StringReader } from "https://deno.land/std@0.167.0/io/readers.ts";
-export { StringWriter } from "https://deno.land/std@0.167.0/io/writers.ts";
-export * as asserts from "https://deno.land/std@0.167.0/testing/asserts.ts";
+export { BufReader, BufWriter } from "https://deno.land/std@0.170.0/io/mod.ts";
+export { StringReader } from "https://deno.land/std@0.170.0/io/readers.ts";
+export { StringWriter } from "https://deno.land/std@0.170.0/io/writers.ts";
+export * as asserts from "https://deno.land/std@0.170.0/testing/asserts.ts";
