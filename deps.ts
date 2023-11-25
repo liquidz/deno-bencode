3780b5e4c9cba873463611632028336387d3902b
liquidz <liquidz.uo@gmail.com>
2023-11-25 09:40:11 +0900

    deps: Bump outdated libraries

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * as io from "https://deno.land/std@0.192.0/io/mod.ts";
-export * as asserts from "https://deno.land/std@0.192.0/testing/asserts.ts";
+export * as io from "https://deno.land/std@0.207.0/io/mod.ts";
+export * as asserts from "https://deno.land/std@0.207.0/testing/asserts.ts";
