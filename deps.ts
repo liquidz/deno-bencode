4b42f6dedb63045fac2a09f094ff42b9a7b93649
liquidz <liquidz.uo@gmail.com>
2023-12-29 09:20:18 +0900

    deps: Fix to use assert/mod

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 export * as io from "https://deno.land/std@0.210.0/io/mod.ts";
-export * as asserts from "https://deno.land/std@0.210.0/testing/asserts.ts";
+export * as asserts from "https://deno.land/std@0.210.0/assert/mod.ts";
